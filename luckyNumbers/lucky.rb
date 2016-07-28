c23201723b258f519b5a759b84923a4be352e6ca
michaeldumalag <michaeldumalag@gmail.com>
2016-07-27 20:30:51 -0700

    Complete lucky numbers method to print set of lucky numbers between range

---
+++ luckyNumbers/lucky.rb
@@ -0,0 +1,48 @@
+class Fixnum
+  def num_digits
+    Math.log10(self).to_i + 1
+  end
+end
+
+# Method will return all the number by digits and place 
+# into an array 
+# input: given integer i.e 1234
+# output: an array of all digits i.e [1,2,3,4]
+def split_digits(number)
+  number.to_s.split(//).map{|chr| chr.to_i}
+end
+
+# Method will add the digits of a given number
+# i.e. 1234 = 1+2+3+4 = 10
+def add_digits(number)
+  nums_to_add = split_digits(number)
+  return nums_to_add.reduce(0, :+)
+end
+
+# This method will recursively add digits until 1 digit
+def successive_addition(number)
+  return number if number.num_digits == 1
+  result_num = add_digits(number)
+
+  if result_num.num_digits == 1
+    return result_num
+  else 
+    successive_addition(result_num)
+  end
+end
+
+def lucky_numbers
+  lowest_number = ARGV[0].to_i
+  highest_number = ARGV[1].to_i
+  lucky_num_range = (lowest_number..highest_number)
+
+  lucky_num_range.each do |number| 
+    potential_lucky_num = successive_addition(number)
+    
+    if potential_lucky_num == 7
+      puts number
+    end
+  end
+end
+
+lucky_numbers
